65e7b6ba2fd30bdfeb41416e9510bc6ea6572692
NIKHILDUGAR <nikhil4709@gmail.com>
2019-10-19 22:27:12 +0530

    first commit

---
+++ Trial/Trial/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/blank', pathMatch: 'full' },
+  {
+    path: 'blank',
+    loadChildren: () => import('./app-shell/blank/blank.module').then(mod => mod.BlankModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
+
