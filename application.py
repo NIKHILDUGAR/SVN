c12300b37f375f898e3ca4e3de01e0f6d1a333f0
generousadmiral <smitsshah8@gmail.com>
2019-10-20 15:00:27 +0530

    rename entrypoint

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask,request
 import Main
+
 app = Flask(__name__)
+
 inputs=""
+
 @app.route("/",methods=["GET","POST"])
 def adder_page():
     if request.method == "POST":
@@ -50,4 +53,4 @@ def adder_page():
          '''
 	
 if __name__ == '__main__':  
-	app.run(debug = True, port=5000, host='0.0.0.0'))     
+	app.run(debug = True)     
